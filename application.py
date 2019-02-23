d55c2f0f175af3fdf8b398f59a4332e98fdd3c1f
Simon Carryer <simoncarryer@gmail.com>
2019-02-23 14:12:28 +1300

    removing encounter ui

--- application.py
+++ application.py
@@ -47,30 +47,5 @@ class Encounter(Resource):
         encounter = source.get_encounter()
         return encounter
 
-@application.route("/encounter-ui", methods=['GET', 'POST'])
-def encounter_ui():
-    xp_budget = 450
-    monster_set = None
-    style = None
-    if request.form:
-        xp_budget = int(request.form['xp_budget'])
-        monster_set = request.form['monster_set']
-        if monster_set == 'None':
-            monster_set = None
-        style = request.form['style']
-        if style == 'all':
-            style = None
-    source = EncounterSource(xp_budget=xp_budget, style=style, monster_sets=[monster_set])
-    sets = source.monster_source.monster_set_names
-    styles = ['elite', 'leader', 'basic', 'all']
-    encounter = source.get_encounter()
-    values = {'xp_budget': xp_budget, 'style': style, 'monster_set': monster_set}
-    return render_template('encounter.html',
-                            encounter=encounter,
-                            values=values,
-                            all_sets=sets,
-                            styles=styles)
-
-
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
